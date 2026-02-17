60ac11acfec177c1961162de4b0ae85ceb2f213b
Vincenzo <vincenzo.delluniversit@virgilio.it>
2026-02-17 23:59:59 +0100

    Gli shot possono distruggere gli asteroidi

--- main.py
+++ main.py
@@ -43,6 +43,12 @@ def main():
                 log_event("player_hit")
                 print("Game Over!")
                 sys.exit()
+
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         
         screen.fill("black")
         for sprite in drawable:
