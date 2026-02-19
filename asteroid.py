fb0289948eeb95eccee962d441d18e6de63ab9d7
Vincenzo <vincenzo.delluniversit@virgilio.it>
2026-02-19 20:46:13 +0100

    Implementa la divisione degli asteroidi quando colpiti

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -8,6 +10,18 @@ class Asteroid(CircleShape):
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, width=LINE_WIDTH)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        new_velocity1 = self.velocity.rotate(random_angle)
+        new_velocity2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        Asteroid(self.position.x, self.position.y, new_radius).velocity = new_velocity1 * 1.2
+        Asteroid(self.position.x, self.position.y, new_radius).velocity = new_velocity2 * 1.2
 
     def update(self, dt):
         self.position += self.velocity * dt
--- circleshape.py
+++ circleshape.py
@@ -14,7 +14,7 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius = radius
     
     def collides_with(self, other):
-        return self.position.distance_to(other.position) < self.radius + other.radius
+        return self.position.distance_to(other.position) <= self.radius + other.radius
 
     def draw(self, screen):
         # must override
